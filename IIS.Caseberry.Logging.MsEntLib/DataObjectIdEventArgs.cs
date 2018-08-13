791f0de453295dde91afb407ff6a98c66425939a
Bratchikov Igor <bratchikov@ics.perm.ru>
2018-08-13 12:27:19 +0500

    Initial commit

---
+++ IIS.Caseberry.Logging.MsEntLib/DataObjectIdEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IIS.Caseberry.Logging.MsEntLib
+{
+    /// <summary>
+    /// Содержит идентификатор data-object
+    /// </summary>
+    public class DataObjectIdEventArgs:EventArgs
+    {
+        /// <summary>
+        /// Id дата-обджекта
+        /// </summary>
+        public Object DataObjectId { get; set; }
+    }
+}
